39cdc38305ec8420ca72328309e7f4375bb86d7f
olivonorman <33961270+olivonorman@users.noreply.github.com>
2025-01-08 18:41:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 console.log('Ejecutando el contenido de main.js');
 console.log('Variable de ambiente MI_VARIABLE: ', process.env.MI_VARIABLE);
+console.log('La variable centralizada es igual a:  ', process.env.VARIABLE_CENTRALIZADA);
